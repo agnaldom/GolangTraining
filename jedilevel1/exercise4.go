ce369897c9db4eef6cba90aef160b890a1703a7e
Victor Faria <victor.faria@vivitech.com.br>
2019-06-18 23:32:12 -0300

    Add exercise 4

---
+++ jedilevel1/exercise4.go
@@ -0,0 +1,26 @@
+/* For This exercise
+1. Create your own type. Have the underlying type be an int. 
+2. Create a VARIABLE of your new TYPE with the IDENTIFIER "x" using the "VAR"
+    keyword
+3. in func main
+    a. print out the value of the variable "x"
+    b. print out the type of the variable "x"
+    c. assign 42 to the VARIABLE "x" using the "=" OPERATOR
+    d. print out the value of the variable "x"
+*/
+
+package main
+
+import (
+    "fmt"
+)
+
+type hotdog int
+var x hotdog
+
+func main() {
+    fmt.Println(x) //a
+    fmt.Printf("%T\n", x) //b
+    x=42
+    fmt.Println(x)
+}
